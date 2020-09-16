8b1416421f92d915ed78ab55165a52b56fbd63d7
doughnut <54584786+doughnuty@users.noreply.github.com>
2020-09-16 15:45:59 +0600

    Update methods.go

--- methods.go
+++ methods.go
@@ -1,139 +1,138 @@
 package parser
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"strconv"
 	"time"
 )
 
-func (ans Answer) Bool(def bool) (bool, error) {
+func (ans Answer) Bool(def bool) bool {
 	// if empty panic
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// else convert (if incompatible strconv will panic)
 	ret, err := strconv.ParseBool(temp)
 	if err != nil {
-		return def, errors.New("incompatible types")
+		return def
 	}
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) Int(def int) (int, error) {
+func (ans Answer) Int(def int) int {
 	// if empty panic
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// else convert (if incompatible strconv will panic)
 	ret, err := strconv.Atoi(temp)
 	if err != nil {
-		return def, err
+		return def
 	}
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) String(def string) (ret string, err error) {
+func (ans Answer) String(def string) (ret string) {
 	ret = fmt.Sprintf("%v", ans.val)
 	if ret == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// if not a string notify
 	if reflect.TypeOf(ans.val) != reflect.TypeOf(ret) {
 		fmt.Println("Mismatched types", reflect.TypeOf(ans.val), "and", reflect.TypeOf(ret))
-		return def, errors.New("incompatible types")
+		return def
 	}
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) Float64(def float64) (float64, error) {
+func (ans Answer) Float64(def float64) float64 {
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// else convert (if incompatible strconv will panic)
 	ret, err := strconv.ParseFloat(temp, 64)
 	if err != nil {
-		return def, err
+		return def
 	}
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) Duration(def time.Duration) (time.Duration, error) {
+func (ans Answer) Duration(def time.Duration) time.Duration {
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// else convert
 	ret, err := time.ParseDuration(temp)
 	if err != nil {
-		return def, err
+		return def
 	}
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) StringSlice(def []string) ([]string, error) {
+func (ans Answer) StringSlice(def []string) []string {
 	var ret []string
 	someInterface := new([]interface{})
 
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// if not a slice return
 	if reflect.TypeOf(ans.val) != reflect.TypeOf(*someInterface) {
-		return def, errors.New("mismatched types")
+		return def
 	}
 
 	for i, val := range ans.val.([]interface{}) {
 		// if not a string return
 		if reflect.TypeOf(ans.val.([]interface{})[i]) != reflect.TypeOf(temp) {
-			return def, errors.New("mismatched types")
+			return def
 		}
 		// if string append
 		ret = append(ret, val.(string))
 	}
 
-	return ret, nil
+	return ret
 }
 
-func (ans Answer) StringMap(def map[string]interface{}) (map[string]interface{}, error) {
+func (ans Answer) StringMap(def map[string]interface{}) map[string]interface{} {
 	ret := new(map[string]interface{})
 
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return def, errors.New("no match")
+		return def
 	}
 
 	// if not an Object return
 	var tempObject Object
 
 	if reflect.TypeOf(ans.val) != reflect.TypeOf(tempObject) {
-		return def, errors.New("mismatched types")
+		return def
 	}
 
 	// if Object save Object.Objects value
 	tempObject = ans.val.(Object)
 	*ret = tempObject.Objects
 
-	return *ret, nil
+	return *ret
 }
 
-func (ans Answer) Bytes() ([]byte, error) {
+func (ans Answer) Bytes() []byte {
 	// if empty panic
 	temp := fmt.Sprintf("%v", ans.val)
 	if temp == "<nil>" {
-		return nil, errors.New("no match")
+		return nil
 	}
 
-	return []byte(temp), nil
+	return []byte(temp)
 }
