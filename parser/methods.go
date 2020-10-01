19f2497490bc0c8f811560ce3c56ba303826f65f
doughnuty <malika240502@gmail.com>
2020-10-01 19:02:25 +0600

    Working samples Add more methods

--- parser/methods.go
+++ parser/methods.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 type Mod interface {
 	Bool(def bool) bool
 	Int(def int) int
 	String(def string) string
-	/*	Float64(def float64) float64
-		Duration(def time.Duration) time.Duration
-		StringSlice(def []string) []string
-		StringMap(def map[string]interface{}) map[string]interface{}
-		Bytes() []byte
-	*/
+	Float64(def float64) float64
+	Duration(def time.Duration) time.Duration
+	/*		StringSlice(def []string) []string*/
+	StringMap(def map[string]string) map[string]string
+	Bytes() []byte
 }
 
 func (prop Property) Bool(def bool) bool {
@@ -45,13 +45,61 @@ func (prop Property) Int(def int) int {
 }
 
 func (prop Property) String(def string) string {
-	ret := fmt.Sprintf("%v", prop.Val)
-	if ret == "<nil>" {
+	if prop.Mod != VAL_MOD {
 		return def
 	}
+	ret := fmt.Sprintf("%v", prop.Val)
 	// if not a string notify
 	if reflect.TypeOf(prop.Val) != reflect.TypeOf(ret) {
 		return def
 	}
 	return ret
+}
+
+func (prop Property) Float64(def float64) float64 {
+	if prop.Mod != VAL_MOD {
+		return def
+	}
+
+	temp := fmt.Sprintf("%v", prop.Val)
+
+	// else convert
+	ret, err := strconv.ParseFloat(temp, 64)
+	if err != nil {
+		return def
+	}
+	return ret
+}
+
+func (prop Property) Duration(def time.Duration) time.Duration {
+	if prop.Mod != VAL_MOD {
+		return def
+	}
+	temp := fmt.Sprintf("%v", prop.Val)
+	ret, err := time.ParseDuration(temp)
+	if err != nil {
+		return def
+	}
+	return ret
+}
+
+func (prop Property) StringMap(def map[string]string) map[string]string {
+	if prop.Mod != MAP_MOD {
+		return def
+	}
+	ret := make(map[string]string, 10)
+	for i, j := range prop.Val.(Yaml).Map {
+		if j.Mod != VAL_MOD {
+			return def
+		}
+		ret[i] = j.Val.(string)
+	}
+	return ret
+}
+
+func (prop Property) Bytes() []byte {
+	b, ok := prop.Val.([]byte)
+	fmt.Println(ok)
+	fmt.Println(b)
+	return b
 }
--- test/test.go
+++ test/test.go
@@ -12,6 +12,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(yaml.Get("student", "personal").String("Name"))
+		fmt.Println(yaml.Get("student", "personal").StringMap(nil))
 	}
 }
